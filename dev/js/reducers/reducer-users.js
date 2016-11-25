f0baad8c3b813e7bf11878c1a7236ba3261e52d2
JackTran791 <congsonit@gmail.com>
2016-11-25 12:16:48 -0600

    first commit

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,36 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: "Jack",
+      last: "Tran",
+      age: 25,
+      description: "Jack Tran Awesome",
+      image: "https://www.google.com/search?q=bill+gates&safe=active&rlz=1C1CHZL_enUS718US718&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjIub3LssTQAhXEzVQKHQluBCUQ_AUICSgC&biw=958&bih=930#imgrc=HEy-qFAGO83ILM%3A"
+    },
+    {
+      id: 2,
+      first: "Vicent",
+      last: "Tran",
+      age: 22,
+      description: "Jack Tran Perfect",
+      image: "https://www.google.com/search?q=mark+zuckerberg&safe=active&rlz=1C1CHZL_enUS718US718&biw=958&bih=930&tbm=isch&source=lnt&tbs=isz:ex,iszw:400,iszh:400#imgrc=Xe9Eti-dXGRroM%3A"
+    },
+    {
+      id: 3,
+      first: "Luyen",
+      last: "Tran",
+      age: 49,
+      description: "Jack Tran Very Good",
+      image: "https://www.google.com/search?q=mark+zuckerberg&safe=active&rlz=1C1CHZL_enUS718US718&biw=958&bih=930&tbm=isch&source=lnt&tbs=isz:ex,iszw:400,iszh:400#safe=active&tbs=isz:ex%2Ciszw:400%2Ciszh:400&tbm=isch&q=steve+jobs&imgrc=cbhC_UWLCu59IM%3A"
+    },
+    {
+      id: 4,
+      first: "My",
+      last: "Nguyen",
+      age: 48,
+      description: "Jack Tran Good",
+      image: "https://www.google.com/search?q=mark+zuckerberg&safe=active&rlz=1C1CHZL_enUS718US718&biw=958&bih=930&tbm=isch&source=lnt&tbs=isz:ex,iszw:400,iszh:400#safe=active&tbs=isz:ex%2Ciszw:400%2Ciszh:400&tbm=isch&q=donal+trump&imgrc=UQL0-SGszRZIQM%3A"
+    }
+  ]
+}
